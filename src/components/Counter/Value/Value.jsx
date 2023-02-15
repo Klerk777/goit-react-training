f26832516598895d622f616fe0b1889a98f44045
Klerk777 <Klerk.mail@gmail.com>
2023-02-15 20:26:20 +0000

    Deploying to gh-pages from @ Klerk777/goit-react-training@08f53fa27fedbe5a3ac7077a134df09e038103b9 🚀

--- src/components/Counter/Value/Value.jsx
+++
@@ -1,5 +0,0 @@
-import React from "react";
-import styles from "./Value.module.scss";
-
-const Value = ({ value }) => <span className={styles.result}>{value}</span>;
-export default Value;
--- src/components/Greating/getName.js
+++
@@ -1,4 +0,0 @@
-export default function getName() {
-  const userName = prompt("Enter your name:");
-  return userName;
-}
